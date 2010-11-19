d6d362f6009bb8f14a9ccaf694af885592f065ac
Ellis Berner <eberner@gmail.com>
2010-11-19 00:40:44 -0500

    fixer

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,15 +3,38 @@ class Player < ActiveRecord::Base
   belongs_to :team
   belongs_to :user
 
+  def calculate_opp
+    self.opp = self.shots.count.zero? ? 0 : self.points.to_f / self.shots.count.to_f
+    save!
+  end
+
+  def calculate_points
+    self.points = 0
+    self.shots.each do |s|
+      self.points += 3 if s.cup == 10
+      self.points += 2 if [4, 7, 8, 9].include?(s.cup)
+      self.points += 1 if [1, 2, 3, 5, 6].include?(s.cup)
+    end
+    save!
+  end
+
+  def calculate_hit_percentage
+    self.hit_percentage = self.shots.count.zero? ? 0 : (self.shots.where("cup != 0").count.to_f / self.shots.count.to_f) * 100
+    save!
+  end
+
+  def calculate_assholes
+
+  end
+
   def team_size
     errors[:base] << "Three members per team maximum" if team.players.count > 3
   end
 
-  def award(however_many)
-    points = self.points += however_many
-    self.points = points
-    self.opp = (points.to_f / self.shots.count.to_f)
-    self.hit_percentage = (self.shots.where("cup != 0").count.to_f / self.shots.count.to_f) * 100
+  def award
+    calculate_points
+    calculate_opp
+    calculate_hit_percentage
     save!
   end
   
