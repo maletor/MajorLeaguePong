7befbadfbcaf938fe06b6fe38d0f59fad58b1514
Ellis Berner <eberner@gmail.com>
2010-11-21 20:48:42 -0500

    application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   def fucking_advice
     ["Fucking ideate", "Use more fucking white space", "Fucking start over", "Read the fucking copy", "Capitalize on the fucking experience", "Use fucking alignment", "Less if fucking more", "Learn from your fucking mistakes", "There is no fucking box", "Pay attention to fucking detail", "Think fucking laterally", "Quit your fucking job", "Write a fucking design brief", "Consider your fucking user", "Fucking do it again", "Sans-fucking-serif", "Fucking experiement", "Get over your fucking self", "Work outside of your fucking habits", "Do it right the first fucking time", "Give a damn", "Learn from your fucking peers", "Love your job", "Fucking simplify", "Take a fucking chance", "Educate your client", "Be a good fucking person", "Don't be trendy", "Have a clear fucking hierarchy"]
   end
